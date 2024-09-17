c91e8333edfc35542e3e289703673065ffaa1ef3
dglee95 <111043356+dglee95@users.noreply.github.com>
2024-09-17 23:28:44 +0900

    Title: ab 사이에 있는 c, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240917/ab 사이에 있는 c/c-between-a-and-b.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        boolean check = false;
+        for (int i = a; i <= b; i++) {
+            if (i % c == 0) {
+                check = true;
+                break;
+            }
+        }
+
+        if (check) {
+            System.out.print("YES");
+        } else {
+            System.out.print("NO");
+        }
+    }
+}
