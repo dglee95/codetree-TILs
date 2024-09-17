ab4153e2327dc81a6fb53b60f64f898729ac8678
dglee95 <111043356+dglee95@users.noreply.github.com>
2024-09-17 23:33:22 +0900

    Title: 공약수의 유무, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240917/공약수의 유무/presence-or-absence-of-a-common-divisor.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        boolean check = false;
+        for (int = a; i <= b; i++) {
+            if (1920 % i == 0 && 2880 % i == 0) {
+                check = true;
+                break;
+            }
+        }
+
+        System.out.print(check ? 1 : 0);
+    }
+}
