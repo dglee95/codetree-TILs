b510ee5df71e1d231547039d149f401bafaefef4
dglee95 <111043356+dglee95@users.noreply.github.com>
2024-09-19 14:21:30 +0900

    Title: 숫자 피라미드 3, Time: 394ms, Memory: 16MB, Status: Passed - CodeTree

--- 240919/숫자 피라미드 3/number-pyramid-3.java
+++ 240919/숫자 피라미드 3/number-pyramid-3.java
@@ -9,7 +9,7 @@ public class Main {
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < i + 1; j++) {
-                System.out.printf("%d ", i * j);
+                System.out.printf("%d ", (i + 1) * (j + 1));
             }
             System.out.println();
         }
