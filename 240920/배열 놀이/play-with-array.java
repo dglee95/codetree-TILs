670bc34712e8d81b6147d788a9eff97fe5947f3e
dglee95 <111043356+dglee95@users.noreply.github.com>
2024-09-20 22:15:46 +0900

    Title: 배열 놀이, Time: 198ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 240920/배열 놀이/play-with-array.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int q = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < q; i++) {
+            int type = sc.nextInt();
+
+            if (type == 1) {
+                int a = sc.nextInt();
+                System.out.printf("%d\n", arr[a - 1]);
+            } else if (type == 2) {
+                int b = sc.nextInt();
+                int idx = -1;
+                for (int j = 0; j < n; j++) {
+                    if (arr[j] == b) {
+                        idx = j;
+                        break;
+                    }
+                }
+                if (idx > 0) {
+                    System.out.println(idx + 1);
+                } else {
+                    System.out.println(0);
+                }
+            } else {
+                int s = sc.nextInt();
+                int e = sc.nextInt();
+                for (int j = s; j <= e; j++) {
+                    System.out.printf("%d ", arr[j - 1]);
+                }
+            }
+        }
+    }
+}
