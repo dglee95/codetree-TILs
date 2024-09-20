bb29cfedc357f39e250d2846f95926cc6aaae69b
dglee95 <111043356+dglee95@users.noreply.github.com>
2024-09-20 22:50:34 +0900

    Title: 연속부분수열일까, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240920/연속부분수열일까/contiguous-array-or-not.java
+++ 240920/연속부분수열일까/contiguous-array-or-not.java
@@ -18,31 +18,62 @@ public class Main {
             b[i] = sc.nextInt();
         }
 
-        int[] idxArr = new int[n2];
-        for (int i = 0; i < n2; i++) {
-            for (int j = i; j < n1; j++) {
-                idxArr[i] = -1;
-                if (a[j] == b[i]) {
-                    idxArr[i] = j;
-                    break;
-                }
+        int idx = -1;
+        for (int i = 0; i < n1 - n2; i++) {
+            if (a[i] == b[0]) {
+                idx = i;
+                break;
             }
         }
 
-        boolean check = true;
-        if (idxArr[0] > -1) {
-            for (int i = 1; i < n2; i++) {
-                if (idxArr[i] != idxArr[i - 1] + 1) {
+        if (idx > -1) {
+            boolean check = true;
+            for (int i = 0; i < n2; i++) {
+                if (b[i] != a[idx++]) {
                     check = false;
+                    break;
                 }
             }
-            if (check) 
+            if (check)
                 System.out.print("Yes");
             else
                 System.out.print("No");
+
         } else {
             System.out.print("No");
         }
+
+
+
+        // for (int i = 0; i < n2; i++) {
+        //     for (int j = i; j < n1; j++) {
+        //         idxArr[i] = -1;
+        //         if (a[j] == b[i]) {
+        //             idxArr[i] = j;
+        //             break;
+        //         }
+        //     }
+        // }
+
+        // for (int i = 0; i < n2; i++) {
+        //     System.out.printf("%d ", idxArr[i]);
+        // }
+        // System.out.println();
+
+        // boolean check = true;
+        // if (idxArr[0] > -1) {
+        //     for (int i = 1; i < n2; i++) {
+        //         if (idxArr[i] != idxArr[i - 1] + 1) {
+        //             check = false;
+        //         }
+        //     }
+        //     if (check) 
+        //         System.out.print("Yes");
+        //     else
+        //         System.out.print("No");
+        // } else {
+        //     System.out.print("No");
+        // }
 
         
     }
