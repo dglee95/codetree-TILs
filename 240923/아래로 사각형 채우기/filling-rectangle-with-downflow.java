74c6048c6cbac4128dc3ea6a7c90a3308ce71dea
dglee95 <111043356+dglee95@users.noreply.github.com>
2024-09-23 16:28:22 +0900

    Title: 아래로 사각형 채우기, Time: 178ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240923/아래로 사각형 채우기/filling-rectangle-with-downflow.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+        int cnt = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[j][i] = cnt++;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.printf("%d ", arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
