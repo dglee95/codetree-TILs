1cb5e32f1b955542a735957d15c9ecbe87dabe98
dglee95 <111043356+dglee95@users.noreply.github.com>
2024-09-23 17:38:01 +0900

    Title: 배열로 사각형 만들기, Time: 121ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240923/배열로 사각형 만들기/print-array-in-rectangle.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int[][] arr = new int[5][5];
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (i == 0 || j == 0) {
+                    arr[i][j] = 1;
+                } else {
+                    arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
+                }
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.printf("%d ", arr[i][j]);
+            }
+            System.out.println();
+        }
+        
+    }
+}
