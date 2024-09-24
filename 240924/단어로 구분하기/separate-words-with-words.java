790f8eda9ff7e594da06e61d474ee518329e2178
dglee95 <111043356+dglee95@users.noreply.github.com>
2024-09-24 09:53:12 +0900

    Title: 단어로 구분하기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240924/단어로 구분하기/separate-words-with-words.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != ' ') {
+                System.out.print(str.charAt(i));
+            } else {
+                System.out.println();
+            }
+        }
+    }
+}
