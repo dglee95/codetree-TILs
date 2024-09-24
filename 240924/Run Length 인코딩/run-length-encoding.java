cdd805e66860fee9857f9d6f59467ed2990a2750
dglee95 <111043356+dglee95@users.noreply.github.com>
2024-09-24 13:24:11 +0900

    Title: Run Length 인코딩, Time: 116ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240924/Run Length 인코딩/run-length-encoding.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.lang.StringBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String A = sc.next();
+        char c = A.charAt(0);
+
+        StringBuilder builder = new StringBuilder();
+        int cnt = 1;
+
+        int len = 0;
+
+        for (int i = 1; i < A.length(); i++) {
+            if (A.charAt(i - 1) == A.charAt(i)) {
+                cnt++;
+            } else {
+                builder.append(c);
+                builder.append(cnt);
+                cnt = 1;
+                c = A.charAt(i);
+            }
+
+            if (i == A.length() - 1) {
+                builder.append(c);
+                builder.append(cnt);
+            }
+        }
+        System.out.println(builder.length());
+        System.out.println(builder);
+    }
+}
